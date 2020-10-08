b6fefa982db38ec97a1833ade76454ab4a98eb0c
legend19410 <francismilton19410@gmail.com>
2020-10-08 16:48:07 -0500

    There is a winner

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,6 +1,7 @@
 document.addEventListener('DOMContentLoaded', function (){
     const board = document.getElementById('board');
     const boardChildern = Array.from(board.children);
+    const statusMessage = document.getElementById('status');
     let turn = 0;
     let gameState = [['0', '1', '2'], 
                      ['3', '4', '5'],
@@ -15,9 +16,13 @@ document.addEventListener('DOMContentLoaded', function (){
                         box.classList.add("X");
                         box.textContent = "X";
                         let id_index = state.indexOf(id);
-                        state[id_index] = true;
+                        state[id_index] = 'X';
                         turn = 1;
                         console.log(gameState);
+                        if(isAWinner(gameState,'X')){
+                            statusMessage.innerHTML = "Congratulations! X is the Winner!";
+                            statusMessage.classList.add("you-won");
+                        }
                         break;
                     }
                 }
@@ -27,9 +32,14 @@ document.addEventListener('DOMContentLoaded', function (){
                         box.classList.add("O");
                         box.textContent = "O";
                         let id_index = state.indexOf(id);
-                        state[id_index] = false;
+                        state[id_index] = 'O';
                         turn = 0;
                         console.log(gameState);
+                        if(isAWinner(gameState,'O')){
+                            statusMessage.innerHTML = "Congratulations! O is the Winner!";
+                            statusMessage.classList.add("you-won");
+                        }
+                        
                         break;
                     }
                 }
@@ -45,6 +55,27 @@ document.addEventListener('DOMContentLoaded', function (){
         
         box.classList.add('square');
         box.setAttribute('id', index);
+
+        function isAWinner(gS,player){
+            for(let i=0; i<gS.length; i++){
+                if(gS[i][0] === player && gS[i][1]=== player && gS[i][2]=== player){
+                    return true;
+                }
+                if(gS[0][i]=== player && gS[1][i]=== player && gS[2][i]=== player){
+                    return true;
+                }
+                
+            }
+
+            if(gS[0][0]=== player && gS[1][1]=== player && gS[2][2]=== player){
+                return true;
+            }
+
+            if(gS[0][2]=== player && gS[1][1]=== player && gS[2][0]=== player){
+                return true;
+            }
+      
+        }
            
         
         
